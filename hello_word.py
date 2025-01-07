1bd52d50a41d43ad046894cf8a3510307d26938c
Haole <lehaoss1412@gmail.com>
2025-01-07 23:04:32 +0700

    300 bai code thieu nhi

--- hello_word.py
+++ hello_word.py
@@ -1,4 +1,18 @@
 #greeting
-print('Hello world!')
+#Two sum
+class Solution:
+  def twoSum(self, nums, target):
+      hashmap = {}
+      for i, num in enumerate(nums):
+        complement = target - num
+        if complement in hashmap:
+            return [hashmap[complement], i]
+        hashmap[num] = i
+      return []
+ 
+solution = Solution()
 
-print('Hello everyone!')
+test_case_1 = [2, 7, 11, 15]
+target_1 = 9
+result_1 = solution.twoSum(test_case_1, target_1)
+print(f"Test case result: {result_1}")
